560ef35a1dddee85d2a62d56556ddcf85a61ce2b
Созыкин Родион <skykanade@gmail.com>
2024-09-17 23:32:54 +0500

    add create superuser

--- notes/entrypoint.sh
+++ notes/entrypoint.sh
@@ -4,5 +4,14 @@
 # Run migrations
 python manage.py migrate
 
+#create superuser
+if [ "$DJANGO_SUPERUSER_USERNAME" ]
+then
+    python manage.py createsuperuser \
+        --noinput \
+        --username $DJANGO_SUPERUSER_USERNAME \
+        --email $DJANGO_SUPERUSER_EMAIL
+fi
+
 # Then run the main container command (passed to us as arguments)
 exec "$@"
